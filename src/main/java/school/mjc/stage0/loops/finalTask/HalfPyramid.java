f93fbe78fff2527e960a6745e4fcb77953568a83
mordim3r <valent1n.bazylev@gmail.com>
2026-06-07 14:42:51 +0300

    ФВВ

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,8 +2,20 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            // пробелы слева
+            for (int j = 1; j <= cathetusLength - i; j++) {
+                System.out.print(" ");
+            }
+            // звёздочки
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
+
+
 
     public static void main(String[] args) {
         new HalfPyramid().printHalfPyramid(8);
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,8 +2,24 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            // пробелы
+            for (int s = 0; s < cathetusLength - i; s++) {
+                System.out.print(" ");
+            }
+            // убывание
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            // возрастание (со 2, чтобы не дублировать 1)
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
+
+
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
